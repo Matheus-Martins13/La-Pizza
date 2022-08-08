0737190f4a3c468f2934835c8a5f7c7a816ac24d
Matheus Martins <matheusmmartins8@gmail.com>
2022-08-08 20:43:14 -0300

    Responsividade, página em inglês, correções, seção Promoções e remoção da busca

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,5 @@
-const readMore = document.querySelector('.read-more');
-const moreContent = document.querySelector('.more-content');
 const send = document.querySelector('.send');
 const message = document.querySelector('.message');
-
-readMore.addEventListener('click', (e) => {
-  moreContent.innerHTML = '<br/> A pizzaria é um patrimônio cultural brasileiro, onde reinventamos a culinária, de modo que só a gente sabe fazer. Fomos avaliados com nota máxima pelos maiores chefs do continente. Nós não somos apenas uma pizzaria, nós somos La Pizza, o lugar onde todo sabor é o sabor da sua vida.';
-  readMore.delete;
-});
 
 send.addEventListener('click', (e) => {
   e.preventDefault();
